bb9628c00355c27f546752fd3b1a18c09fec40c2
natansouzaa <natan.souza@ccc.ufcg.edu.br>
2021-09-28 15:13:39 -0300

    Add use case of specification creation

--- RentalX/src/routes/specifications.routes.ts
+++ RentalX/src/routes/specifications.routes.ts
@@ -1,27 +1,10 @@
 import { Router } from 'express';
-
-import { SpecificationRepository } from '../modules/cars/repositories/SpecificationsRepository';
-import { CreateSpecificationService } from '../modules/cars/services/CreateSpecificationService';
+import { createSpecificationController } from '../modules/cars/useCases/createSpecification';
 
 const specificationsRoutes = Router();
-const specificationsRepository = new SpecificationRepository();
 
 specificationsRoutes.post('/', (request, response) => {
-  const { name, description } = request.body;
-
-  const createSpecificationService = new CreateSpecificationService(
-    specificationsRepository,
-  );
-
-  createSpecificationService.execute({ name, description });
-
-  return response.status(201).send();
-});
-
-specificationsRoutes.get('/', (request, response) => {
-  const allSpecifications = specificationsRepository.list();
-
-  return response.json(allSpecifications);
+  return createSpecificationController.handle(request, response);
 });
 
 export { specificationsRoutes };
