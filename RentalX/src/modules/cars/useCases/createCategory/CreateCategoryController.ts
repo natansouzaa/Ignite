cac89defc41a9bfecc1d774ab5729f18fb2dba4a
natansouzaa <natan.souza@ccc.ufcg.edu.br>
2021-10-04 17:32:48 -0300

    Add and configure TSyring dependency

--- RentalX/src/modules/cars/useCases/createCategory/CreateCategoryController.ts
+++ RentalX/src/modules/cars/useCases/createCategory/CreateCategoryController.ts
@@ -1,18 +1,14 @@
 import { Request, Response } from 'express';
+import { container } from 'tsyringe';
 
 import { CreateCategoryUseCase } from './CreateCategoryUseCase';
 
 class CreateCategoryController {
-  private createCategoryUseCase: CreateCategoryUseCase;
-
-  constructor(createCategoryUseCase: CreateCategoryUseCase) {
-    this.createCategoryUseCase = createCategoryUseCase;
-  }
-
   async handle(request: Request, response: Response): Promise<Response> {
     const { name, description } = request.body;
+    const createCategoryUseCase = container.resolve(CreateCategoryUseCase);
 
-    await this.createCategoryUseCase.execute({ name, description });
+    await createCategoryUseCase.execute({ name, description });
 
     return response.status(201).send();
   }
