6307d41d8bff16c87c7e3514bfe6bd524b908f3b
natansouzaa <natan.souza@ccc.ufcg.edu.br>
2021-10-06 23:01:39 -0300

    Add user authentication

--- RentalX/src/modules/accounts/useCases/createUser/CreateUserController.ts
+++ RentalX/src/modules/accounts/useCases/createUser/CreateUserController.ts
@@ -5,11 +5,16 @@ import { CreateUserUseCase } from './CreateUserUseCase';
 
 class CreateUserController {
   async handle(request: Request, response: Response): Promise<Response> {
-    const { name, password, email, driver_license } = request.body;
+    const { name, email, password, driver_license } = request.body;
 
-    const createUserUserCase = container.resolve(CreateUserUseCase);
+    const createUserUseCase = container.resolve(CreateUserUseCase);
 
-    await createUserUserCase.execute({ name, password, email, driver_license });
+    await createUserUseCase.execute({
+      name,
+      email,
+      password,
+      driver_license,
+    });
 
     return response.status(201).send();
   }
