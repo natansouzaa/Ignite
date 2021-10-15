e259ddcb2c0aa8fc1c19029448ae2eb3ceb56a38
natansouzaa <natan.souza@ccc.ufcg.edu.br>
2021-10-14 21:30:08 -0300

    Add continuation of car creation usecase

--- RentalX/src/modules/cars/useCases/createCar/CreateCarUseCase.spec.ts
+++ RentalX/src/modules/cars/useCases/createCar/CreateCarUseCase.spec.ts
@@ -1,3 +1,4 @@
+import { AppError } from '../../../../shared/errors/AppError';
 import { InMemoryCarsRepository } from '../../repositories/in-memory/InMemoryCarsRepository';
 import { CreateCarUseCase } from './CreateCarUseCase';
 
@@ -11,7 +12,7 @@ describe('Create car', () => {
   });
 
   it('Should be able to create a new car', async () => {
-    await createCarUseCase.execute({
+    const car = await createCarUseCase.execute({
       name: 'Name car',
       description: 'Description car',
       daily_rate: 100,
@@ -20,5 +21,45 @@ describe('Create car', () => {
       brand: 'Brand',
       category_id: 'Category_id',
     });
+
+    expect(car).toHaveProperty('id');
+  });
+
+  it('Shold be not able to create a car with exists license plate', () => {
+    expect(async () => {
+      await createCarUseCase.execute({
+        name: 'Car1',
+        description: 'Description car',
+        daily_rate: 100,
+        license_plate: '1234',
+        fine_amount: 60,
+        brand: 'Brand',
+        category_id: 'Category_id',
+      });
+
+      await createCarUseCase.execute({
+        name: 'Car2',
+        description: 'Description car',
+        daily_rate: 100,
+        license_plate: '1234',
+        fine_amount: 60,
+        brand: 'Brand',
+        category_id: 'Category_id',
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('Shold be able to create a car with available true by defalt', async () => {
+    const car = await createCarUseCase.execute({
+      name: 'Car Available',
+      description: 'Description car',
+      daily_rate: 100,
+      license_plate: 'ABC-1234',
+      fine_amount: 60,
+      brand: 'Brand',
+      category_id: 'Category_id',
+    });
+
+    expect(car.available).toBe(true);
   });
 });
--- RentalX/src/modules/cars/useCases/createCar/CreateCarUseCase.ts
+++ RentalX/src/modules/cars/useCases/createCar/CreateCarUseCase.ts
@@ -1,11 +1,13 @@
 import { inject, injectable } from 'tsyringe';
+import { AppError } from '../../../../shared/errors/AppError';
 import { ICreateCarDTO } from '../../dtos/ICreateCarDTO';
+import { Car } from '../../infra/typeorm/entities/Car';
 import { ICarsRepository } from '../../repositories/ICarsRepository';
 
-@injectable()
+// @injectable()
 class CreateCarUseCase {
   constructor(
-    @inject('CarsRepository')
+    // @inject('CarsRepository')
     private carsRepository: ICarsRepository,
   ) {}
 
@@ -17,8 +19,16 @@ class CreateCarUseCase {
     fine_amount,
     brand,
     category_id,
-  }: ICreateCarDTO): Promise<void> {
-    this.carsRepository.create({
+  }: ICreateCarDTO): Promise<Car> {
+    const carAlreadyExists = await this.carsRepository.findByLicencePlate(
+      license_plate,
+    );
+
+    if (carAlreadyExists) {
+      throw new AppError('Car already exists!');
+    }
+
+    const car = await this.carsRepository.create({
       name,
       description,
       daily_rate,
@@ -27,6 +37,8 @@ class CreateCarUseCase {
       brand,
       category_id,
     });
+
+    return car;
   }
 }
 
