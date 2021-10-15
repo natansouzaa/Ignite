e86d9df577fdfaf2ba5d5cd1b7648a1ade715f16
natansouzaa <natan.souza@ccc.ufcg.edu.br>
2021-10-15 00:25:09 -0300

    Add list of available cars

--- RentalX/src/modules/cars/repositories/in-memory/InMemoryCarsRepository.ts
+++ RentalX/src/modules/cars/repositories/in-memory/InMemoryCarsRepository.ts
@@ -34,6 +34,24 @@ class InMemoryCarsRepository implements ICarsRepository {
   async findByLicencePlate(license_plate: string): Promise<Car> {
     return this.cars.find(car => car.license_plate === license_plate);
   }
+
+  async findAvailable(
+    brand?: string,
+    name?: string,
+    category_id?: string,
+  ): Promise<Car[]> {
+    const cars = this.cars.filter(car => {
+      if (
+        (car.available === true && brand && car.brand === brand) ||
+        (category_id && car.category_id === category_id) ||
+        (name && car.name === name)
+      ) {
+        return car;
+      }
+      return null;
+    });
+    return cars;
+  }
 }
 
 export { InMemoryCarsRepository };
