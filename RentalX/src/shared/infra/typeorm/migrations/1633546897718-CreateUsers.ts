673b0ff423a424ce4a7a3bf031b248cedfdf0e35
natansouzaa <natan.souza@ccc.ufcg.edu.br>
2021-10-19 14:52:48 -0300

    Add rental migration

--- RentalX/src/shared/infra/typeorm/migrations/1633546897718-CreateUsers.ts
+++ RentalX/src/shared/infra/typeorm/migrations/1633546897718-CreateUsers.ts
@@ -9,6 +9,7 @@ export class CreateUsers1633546897718 implements MigrationInterface {
           {
             name: 'id',
             type: 'uuid',
+            isPrimary: true,
           },
           {
             name: 'name',
