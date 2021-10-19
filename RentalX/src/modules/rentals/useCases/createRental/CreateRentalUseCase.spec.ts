4007ef51088ee472e147c66a78c60e406acf8597
natansouzaa <natan.souza@ccc.ufcg.edu.br>
2021-10-19 17:08:26 -0300

    Add date system with dayjs

--- RentalX/src/modules/rentals/useCases/createRental/CreateRentalUseCase.spec.ts
+++ RentalX/src/modules/rentals/useCases/createRental/CreateRentalUseCase.spec.ts
@@ -1,3 +1,5 @@
+import dayjs from 'dayjs';
+
 import { AppError } from '../../../../shared/errors/AppError';
 import { InMemoryRentalsRepository } from '../../repositories/in-memory/InMemoryRentalsRepository';
 import { CreateRentalUseCase } from './CreateRentalUseCase';
@@ -6,50 +8,61 @@ let createRentalUseCase: CreateRentalUseCase;
 let inMemoryRentalsRepository: InMemoryRentalsRepository;
 
 describe('Create rental', () => {
+  const dayAdd24Hours = dayjs().add(1, 'day').toDate();
   beforeEach(() => {
     inMemoryRentalsRepository = new InMemoryRentalsRepository();
     createRentalUseCase = new CreateRentalUseCase(inMemoryRentalsRepository);
   });
 
-  it('Shold be able to create a new rental', async () => {
+  it('Should be able to create a new rental', async () => {
     const rental = await createRentalUseCase.execute({
       user_id: '12345',
       car_id: '121212',
-      expected_return_date: new Date(),
+      expected_return_date: dayAdd24Hours,
     });
 
     expect(rental).toHaveProperty('id');
     expect(rental).toHaveProperty('start_date');
   });
 
-  it('Shold not be able to create a new rental if there is another open to the same user', async () => {
+  it('Should not be able to create a new rental if there is another open to the same user', async () => {
     expect(async () => {
       await createRentalUseCase.execute({
         user_id: '12345',
         car_id: '121212',
-        expected_return_date: new Date(),
+        expected_return_date: dayAdd24Hours,
       });
 
       await createRentalUseCase.execute({
         user_id: '12345',
         car_id: '121212',
-        expected_return_date: new Date(),
+        expected_return_date: dayAdd24Hours,
       });
     }).rejects.toBeInstanceOf(AppError);
   });
 
-  it('Shold not be able to create a new rental if there is another open to the same car', async () => {
+  it('Should not be able to create a new rental if there is another open to the same car', async () => {
     expect(async () => {
       await createRentalUseCase.execute({
         user_id: '12345',
         car_id: 'test',
-        expected_return_date: new Date(),
+        expected_return_date: dayAdd24Hours,
       });
 
       await createRentalUseCase.execute({
         user_id: '54321',
         car_id: 'test',
-        expected_return_date: new Date(),
+        expected_return_date: dayAdd24Hours,
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('Should not be able to create a new rental with invalid return time', async () => {
+    expect(async () => {
+      await createRentalUseCase.execute({
+        user_id: '123',
+        car_id: 'test',
+        expected_return_date: dayjs().toDate(),
       });
     }).rejects.toBeInstanceOf(AppError);
   });
