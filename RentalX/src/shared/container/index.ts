db0a70601819babd1a5aa48cbe011dae11893a78
natansouzaa <natan.souza@ccc.ufcg.edu.br>
2021-10-19 00:37:14 -0300

    Add migration and usecase of car images

--- RentalX/src/shared/container/index.ts
+++ RentalX/src/shared/container/index.ts
@@ -8,6 +8,8 @@ import { SpecificationsRepository } from '../../modules/cars/infra/typeorm/repos
 import { ISpecificationsRepository } from '../../modules/cars/repositories/ISpecificationsRepository';
 import { ICarsRepository } from '../../modules/cars/repositories/ICarsRepository';
 import { CarsRepository } from '../../modules/cars/infra/typeorm/repositories/CarsRepository';
+import { ICarsImagesRepository } from '../../modules/cars/repositories/ICarsImagesRepository';
+import { CarsImagesRepository } from '../../modules/cars/infra/typeorm/repositories/CarsImagesRepository';
 
 container.registerSingleton<ICategoriesRepository>(
   'CategoriesRepository',
@@ -22,6 +24,11 @@ container.registerSingleton<ISpecificationsRepository>(
 container.registerSingleton<IUsersRepository>(
   'UsersRepository',
   UsersRepository,
+);
+
+container.registerSingleton<ICarsImagesRepository>(
+  'CarsImagesRepository',
+  CarsImagesRepository,
 );
 
 container.registerSingleton<ICarsRepository>('CarsRepository', CarsRepository);
--- RentalX/src/shared/infra/http/routes/cars.routes.ts
+++ RentalX/src/shared/infra/http/routes/cars.routes.ts
@@ -1,16 +1,22 @@
 import { Router } from 'express';
+import multer from 'multer';
 
 import { CreateCarController } from '../../../../modules/cars/useCases/createCar/CreateCarController';
 import { CreateCarSpecificationController } from '../../../../modules/cars/useCases/createCarSpecification/CreateCarSpecificationController';
 import { ListAvailableCarsController } from '../../../../modules/cars/useCases/listAvailableCars/ListAvailableCarsController';
+import { UploadCarImagesController } from '../../../../modules/cars/useCases/uploadCarImages/UploadCarImagesController';
 import { ensureAdmin } from '../middlewares/ensureAdmin';
 import { ensureAuthenticated } from '../middlewares/ensureAuthenticated';
+import uploadConfig from '../../../../config/upload';
 
 const carsRoutes = Router();
 
 const createCarController = new CreateCarController();
 const listAvailableCarsController = new ListAvailableCarsController();
 const createCarSpecificationController = new CreateCarSpecificationController();
+const uploadCarImagesController = new UploadCarImagesController();
+
+const upload = multer(uploadConfig.upload('./tmp/cars'));
 
 carsRoutes.post(
   '/',
@@ -26,6 +32,14 @@ carsRoutes.post(
   ensureAuthenticated,
   ensureAdmin,
   createCarSpecificationController.handle,
+);
+
+carsRoutes.post(
+  '/images/:id',
+  ensureAuthenticated,
+  ensureAdmin,
+  upload.array('images'),
+  uploadCarImagesController.handle,
 );
 
 export { carsRoutes };
